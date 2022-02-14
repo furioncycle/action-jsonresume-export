56f67a29c289728eefe21b2747b0ceac0d62502c
ttecho <admarquez21@gmail.com>
2022-02-13 17:19:09 -0800

    Removed pdf ability

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,12 @@
 THEME=${1}
 RESUME=${2}
 OUTPUT=${3}
-PDF=${4}
 
 THEME_PACKAGE=jsonresume-theme-${THEME}
 echo "Installing theme: ${THEME}"
 
 # NOTE: this needs to be installed locally, not globally
 npm install ${THEME_PACKAGE}
-
-echo "Exporting PDF..."
-resume export --resume ${RESUME} --theme ${THEME} --format pdf ${PDF}
 
 echo "Exporting HTML..."
 resume export --resume ${RESUME} --theme ${THEME} --format html ${OUTPUT}
